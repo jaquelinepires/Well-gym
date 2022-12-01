57ace2037f7a63a923303f3c5d4d58a3f751fd84
Jaqueline <piresjaqueline17@gmail.com>
2022-12-01 16:24:46 -0300

    feat: Creating hook to facilitate context consumption

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -4,6 +4,22 @@ import { UserDTO } from "../dtos/UserDTO";
 export type AuthContextDataProps = {
   user: UserDTO;
 }
+type AuthContextProviderProps = {
+  children: React.ReactNode;
+}
 export const AuthContext = createContext({} as AuthContextDataProps);
 
-
+export function AuthContextProvider({children}: AuthContextProviderProps) {
+  return (
+    <AuthContext.Provider value={{
+      user: {
+      id: '1',
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      avatar: 'john.png'
+      }
+    }}>
+      { children }
+    </AuthContext.Provider>
+  )
+}
