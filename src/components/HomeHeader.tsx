d6f35aec344d36819625f8fb6ac4f1e67c409639
Jaqueline <piresjaqueline17@gmail.com>
2022-12-06 22:59:40 -0300

    feat: Creating the signOut method

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -6,7 +6,7 @@ import { MaterialIcons } from '@expo/vector-icons';
 import { useAuth } from '../hooks/useAuth';
 
 export function HomeHeader() {
-  const { user, signOut } = useAuth();
+  const { user } = useAuth();
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <UserPhoto 
@@ -25,7 +25,7 @@ export function HomeHeader() {
         </Heading>
       </VStack>
 
-      <TouchableOpacity onPress={signOut}>
+      <TouchableOpacity>
         <Icon 
           as={MaterialIcons}
           name="logout"
--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,14 +1,12 @@
 import { createContext, useEffect, useState } from "react";
 import { UserDTO } from "../dtos/UserDTO";
 import { api } from "../services/api";
-import { storageUserSave, storageUserGet, storageUserRemove } from "../storage/storageUser";
+import { storageUserSave, storageUserGet } from "../storage/storageUser";
 
 export type AuthContextDataProps = {
   user: UserDTO;
   signIn: (email: string, password: string) => Promise<void>;
-  signOut: () => Promise<void>;
   isLoadingUserStorageData: boolean;
-
 }
 type AuthContextProviderProps = {
   children: React.ReactNode;
@@ -29,19 +27,6 @@ export function AuthContextProvider({children}: AuthContextProviderProps) {
     }
     } catch (error) {
       throw error
-    }
-  }
-  async function signOut() {
-    try {
-      setIsLoadingUserStorageData(true)
-      setUser({} as UserDTO)
-      
-      await storageUserRemove()
-
-    } catch (error) {
-      throw error
-    } finally {
-      setIsLoadingUserStorageData(false)
     }
   }
     async function loadUserData() {
@@ -65,7 +50,6 @@ export function AuthContextProvider({children}: AuthContextProviderProps) {
     <AuthContext.Provider value={{
       user,
       signIn,
-      signOut,
       isLoadingUserStorageData
       }}>
       { children }
