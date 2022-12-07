ceb7c27eb8298188a84226ad6c0fc7e9c02812c4
Jaqueline <piresjaqueline17@gmail.com>
2022-12-06 22:33:22 -0300

    feat: Displaying Loading while loading user

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -6,6 +6,7 @@ import { storageUserGet } from "../storage/storageUser";
 export type AuthContextDataProps = {
   user: UserDTO;
   signIn: (email: string, password: string) => Promise<void>;
+  isLoadingUserStorageData: boolean;
 }
 type AuthContextProviderProps = {
   children: React.ReactNode;
@@ -14,7 +15,8 @@ export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({children}: AuthContextProviderProps) {
   const [user, setUser ] = useState<UserDTO>({} as UserDTO);
-
+  const [ isLoadingUserStorageData, setIsLoadingUserStorageData ] = useState(true)
+  
   async function signIn(email: string, password: string) {
     try{
     const { data } = await api.post('/sessions', {email, password})
@@ -27,18 +29,28 @@ export function AuthContextProvider({children}: AuthContextProviderProps) {
     }
   }
     async function loadUserData() {
+      try{
       const userLogged = await storageUserGet()
 
       if(userLogged) {
         setUser(userLogged)
+        setIsLoadingUserStorageData(false)
       }
+    } catch (error) {
+      throw error
+    } finally {
+      setIsLoadingUserStorageData(false)
     }
-
+  }
     useEffect(() => {
       loadUserData()
     }, [])
   return (
-    <AuthContext.Provider value={{user, signIn}}>
+    <AuthContext.Provider value={{
+      user,
+      signIn,
+      isLoadingUserStorageData
+      }}>
       { children }
     </AuthContext.Provider>
   )
